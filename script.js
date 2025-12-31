4b37ac76e4da6a2e16121364399545b20c5ef322
Rahul-2415 <rahulsingh8171335162@gmail.com>
2025-12-31 21:47:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,36 @@
 function createTable() {
-    //Write your code here
-  
+    const table = document.getElementById("myTable");
+
+    
+    table.innerHTML = "";
+
+    const rn = prompt("Input number of rows");
+    const cn = prompt("Input number of columns");
+
+    
+    if (isNaN(rn) || isNaN(cn)) {
+        return;
+    }
+
+    const rows = Number(rn);
+    const cols = Number(cn);
+
+    
+    if (rows <= 0 || cols <= 0) {
+        alert("Please enter positive numbers greater than zero.");
+        return;
+    }
+
+    
+    for (let i = 0; i < rows; i++) {
+        const tr = document.createElement("tr");
+
+        for (let j = 0; j < cols; j++) {
+            const td = document.createElement("td");
+            td.textContent = `Row-${i} Column-${j}`;
+            tr.appendChild(td);
+        }
+
+        table.appendChild(tr);
+    }
 }
